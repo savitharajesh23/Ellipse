6536c5347d77fa362b7c70eb7a7d8467ee05ca14
Thanesh <thaneshwaran9465@gmail.com>
2023-05-30 07:28:32 -0700

    push code

---
+++ Ellipse.java
@@ -0,0 +1,18 @@
+class Ellipse
+{
+	static void ellipseArea()
+	{
+	int a=10;
+	int b=17;
+	double pie=0.314;
+	double area=pie*a*b;
+	System.out.println(area);
+	}
+ public static void main(String[]args)
+	{
+System.out.println("Start");
+ellipseArea();
+System.out.println("ENDS");
+
+	}
+}
